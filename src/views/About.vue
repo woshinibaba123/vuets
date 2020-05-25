21ec064e7ade83a2bee3c574e66a8c217057ab98
fuxiang <fuxiang@firedata.cn>
2020-05-25 11:24:09 +0800

    updata about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,42 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{page}}</h1>
   </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+    // class Anmai {
+    //   name: string,
+    //   constructor(parameters) {
+    //     console.log(parameters)
+    //   }
+    // }
+export default Vue.extend({
+  data(){
+    return{
+      page: '😁'
+    }
+  },
+  mounted(){
+    // this.test()
+console.log(this.page)
+  },
+  methods:{
+    test(){
+      const name = `fuxiang`
+      this.page = `my name is ${name}`
+      const array: unknown[] =[1,'2',3]
+      array.forEach(vl =>{
+        console.log(vl)
+      })
+      enum color { red='red', blue= 'blue' }
+      const colors: color = color.red
+      console.log(colors)
+      let nosure: unknown = 4
+      nosure = 'fuxioang'
+      console.log(nosure);
+
+    }
+  }
+})
+</script>
